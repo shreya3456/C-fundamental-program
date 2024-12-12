78753cb5c4ae2a7851f2f76d21387a6eed9d224f
shreya3456 <shreyarora012@gmail.com>
2024-12-12 14:28:45 +0530

    Add files via upload

---
+++ enum1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+enum weekdays{Sunday=1, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+int main()
+{
+	enum weekdays w;
+	w = Monday;
+	printf("The value of w is %d",w);
+	return 0;
+}
